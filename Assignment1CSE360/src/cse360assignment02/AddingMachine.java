cef2f51f1661d50d026f219137d09ef934661484
rishabpatgaonkar <rishabpatgaonkar@gmail.com>
2020-10-05 22:43:35 -0700

    created second version.

--- Assignment1CSE360/src/cse360assignment02/AddingMachine.java
+++ Assignment1CSE360/src/cse360assignment02/AddingMachine.java
@@ -6,32 +6,40 @@ package cse360assignment02;
  */
 public class AddingMachine {
 	private int total;
+	String history;
 	  
 	/**Constructor method for AddingMachine**/
 	  public AddingMachine () {
 	    total = 0;  // not needed - included for clarity
+	    history = "0";
 	  }
 	  
 	  /**Accessor method for returning total**/
 	  public int getTotal () {
-	    return 0;
+	    return total;
 	  }
 	  
 	  /** @param value the number to be added to total **/
 	  public void add (int value) {
+		  total = total + value;
+		  history += " + " + value;
 	  }
 
 	  /** @param value the number to be subtracted from total **/
 	  public void subtract (int value) {
+		  history += " - " +  value;
 	  }
 	  
 	  /** toString method to return string representation **/
 	  public String toString () {
-	    return "";
+		  history.toString();
+		  return history;
 	  }
 
 	  /**clear history and total **/
 	  public void clear() {
+		  history = "0";
+		  total = 0;
 	  }
 	
 }
