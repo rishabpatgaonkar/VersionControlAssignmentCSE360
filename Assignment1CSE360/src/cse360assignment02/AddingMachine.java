8abd8492ecbb05daff301e1268ada81e050de768
rishabpatgaonkar <rishabpatgaonkar@gmail.com>
2020-10-05 22:30:55 -0700

    Initial commit of project.

---
+++ Assignment1CSE360/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,37 @@
+package cse360assignment02;
+/**CSE360
+ * Assignment 1 Version Control 
+ * @author Rishab Patgaonkar
+ * Program for adding and subtracting numbers and storing in history
+ */
+public class AddingMachine {
+	private int total;
+	  
+	/**Constructor method for AddingMachine**/
+	  public AddingMachine () {
+	    total = 0;  // not needed - included for clarity
+	  }
+	  
+	  /**Accessor method for returning total**/
+	  public int getTotal () {
+	    return 0;
+	  }
+	  
+	  /** @param value the number to be added to total **/
+	  public void add (int value) {
+	  }
+
+	  /** @param value the number to be subtracted from total **/
+	  public void subtract (int value) {
+	  }
+	  
+	  /** toString method to return string representation **/
+	  public String toString () {
+	    return "";
+	  }
+
+	  /**clear history and total **/
+	  public void clear() {
+	  }
+	
+}
